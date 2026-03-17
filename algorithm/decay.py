d8bd8a35436f924f2a4446aaf66b3bc7abfba6a1
thomasthewalker <thomasthewalker@gmail.com>
2026-03-17 16:16:10 +0100

    bug fixed the new cos_decay function where a () was missing

--- algorithm/decay.py
+++ algorithm/decay.py
@@ -12,7 +12,9 @@ def cos_decay(satisfaction_levels, active_motive, motive_focus, amplitude, eleva
     for i in range(len(satisfaction_levels)):
         if i != active_motive:
             distance = min(
-                abs(i - motive_focus, len(satisfaction_levels) - abs(i - motive_focus))
+                abs(
+                    (i - motive_focus), len(satisfaction_levels) - abs(i - motive_focus)
+                )
             )
             angle = distance * (2 * np.pi / len(satisfaction_levels))
             satisfaction_levels[i] -= amplitude * np.cos(angle) + elevation
