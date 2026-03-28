9226e4349bb81166ccf67cec1833596248d3a188
thomasthewalker <thomasthewalker@gmail.com>
2026-03-28 21:42:06 +0100

    one drive tried to delete all my files, so I had to start a new local repo. hope all works plus tarting a simulation

--- runner_interpersonal.py
+++ runner_interpersonal.py
@@ -17,7 +17,7 @@ _CONFIGURATION_DIRECTION = Path(__file__).resolve().parent / "configuration_file
 #########################
 #########################
 CONFIG = (
-    _CONFIGURATION_DIRECTION / "interpersonal_bigbatch.yaml"
+    _CONFIGURATION_DIRECTION / "ip_bilateral_static_other.yaml"
 )  # <- INPUT YOUR DESIRED CONFIG FILE HERE
 #########################
 #########################
