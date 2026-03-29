82957ab229f379e4bfffc3e4712d840ec0bf9f46
thomasthewalker <thomasthewalker@gmail.com>
2026-03-29 23:30:24 +0200

    deleted some wrong files

--- analysis/interpersonal_film_generation/spiderdiagram_film.py
+++ analysis/interpersonal_film_generation/spiderdiagram_film.py
@@ -7,7 +7,7 @@ import yaml
 
 #########################
 #########################
-RUN_PREFIX = "mehmet_cycle"  # <- INPUT YOUR RUN PREFIX HERE (to filter which runs to make films for)
+RUN_PREFIX = "mehmet_no_cycle"  # <- INPUT YOUR RUN PREFIX HERE (to filter which runs to make films for)
 #########################
 #########################
 
--- runner_interpersonal.py
+++ runner_interpersonal.py
@@ -17,7 +17,7 @@ _CONFIGURATION_DIRECTION = Path(__file__).resolve().parent / "configuration_file
 #########################
 #########################
 CONFIG = (
-    _CONFIGURATION_DIRECTION / "mehmet_cycle.yaml"
+    _CONFIGURATION_DIRECTION / "mehmet_no_cycle.yaml"
 )  # <- INPUT YOUR DESIRED CONFIG FILE HERE
 #########################
 #########################
